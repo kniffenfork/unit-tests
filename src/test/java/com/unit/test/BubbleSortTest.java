b99c6b25593d2b0bca65ab59b9975a76f5b7783d
Алексей Фомин <afomin@fil-it.ru>
2022-04-21 20:31:29 +0300

    test

---
+++ src/test/java/com/unit/test/BubbleSortTest.java
@@ -0,0 +1,17 @@
+package com.unit.test;
+
+import com.unit.test.service.SortService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BubbleSortTest {
+    @Autowired
+    private SortService sortService;
+
+    @Test
+    void blablaTest() {
+        // тестируете
+    }
+}
