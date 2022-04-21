0da11705d3c2b637937cf85e1fa72e22807ce5bb
Алексей Фомин <afomin@fil-it.ru>
2022-04-21 18:40:53 +0300

    init

--- src/test/java/com/unit/test/UnitTestApplicationTests.java
+++ src/test/java/com/unit/test/UnitTestApplicationTests.java
@@ -1,8 +1,6 @@
 package com.unit.test;
 
-import com.unit.test.service.SortService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
